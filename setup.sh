fa22ff9d9af507367f230ee055b1dabd41df5d68
xjmaine <xjfreddie@gmail.com>
2024-08-31 01:47:02 +0100

    rednering setup done

---
+++ setup.sh
@@ -0,0 +1,14 @@
+mkdir -p ~/.streamlit/
+echo "\
+[server]\n\
+headless = true\n\
+port = $PORT\n\
+enableCORS = false\n\
+\n\
+[theme]\n\
+primaryColor='#F63366'\n\
+backgroundColor='#FFFFFF'\n\
+secondaryBackgroundColor='#F0F2F6'\n\
+textColor='#262730'\n\
+font='sans serif'\n\
+" > ~/.streamlit/config.ini
